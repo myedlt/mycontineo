64a0c386d87cfb314b8819bf5171f4485c0ebfa2
zhangml9 <zhangml9@44620f5b-d54f-0410-adb5-515e5ad14fbd>
2008-07-03 10:45:52 +0000

    增加修改常用文档格式的图标 主要针对txt，doc，ppt，xls，pdf，html这六种格式

--- trunk/modules/contineo-core/src/main/java/org/contineo/core/util/IconSelector.java
+++ trunk/modules/contineo-core/src/main/java/org/contineo/core/util/IconSelector.java
@@ -15,20 +15,32 @@ public class IconSelector {
 			icon = "document.gif";
 		else if (ext.equals("pdf"))
 			icon = "pdf.gif";
-		else if (ext.equals("doc") || ext.equals("docx") || ext.equals("dot") || ext.equals("rtf") || ext.equals("sxw") || ext.equals("txt")
+		
+		//zml edit 分离出常用的文件格式及对应图片
+		else if (ext.equals("doc"))
+			icon = "doc.gif";
+		else if (ext.equals("txt"))
+			icon = "txt.gif";
+		else if (ext.equals("ppt"))
+			icon = "ppt.gif";
+		else if (ext.equals("xls"))
+			icon = "xls.gif";
+		else if (ext.equals("htm") || ext.equals("html"))
+			icon = "internet.gif";
+			
+		else if (ext.equals("docx") || ext.equals("dot") || ext.equals("rtf") || ext.equals("sxw") || ext.equals("xml")
 				|| ext.equals("wpd") || ext.equals("kwd") || ext.equals("abw") || ext.equals("zabw") || ext.equals("odt"))
 			icon = "textdoc.gif";
-		else if (ext.equals("xls")|| ext.equals("xslx") || ext.equals("xlt") || ext.equals("sxc") || ext.equals("dbf") || ext.equals("ksp")
+		else if (ext.equals("xslx") || ext.equals("xlt") || ext.equals("sxc") || ext.equals("dbf") || ext.equals("ksp")
 				|| ext.equals("ods") || ext.equals("odb"))
 			icon = "tabledoc.gif";
-		else if (ext.equals("ppt") || ext.equals("pptx") || ext.equals("pps") || ext.equals("pot") || ext.equals("sxi") || ext.equals("kpr")
+		else if (ext.equals("pptx") || ext.equals("pps") || ext.equals("pot") || ext.equals("sxi") || ext.equals("kpr")
 				|| ext.equals("odp"))
 			icon = "presentdoc.gif";
 		else if (ext.equals("apf") || ext.equals("bmp") || ext.equals("cur") || ext.equals("dib") || ext.equals("gif")
 				|| ext.equals("jpg") || ext.equals("psd") || ext.equals("tif") || ext.equals("tiff"))
 			icon = "picture.gif";
-		else if (ext.equals("htm") || ext.equals("html") || ext.equals("xml"))
-			icon = "internet.gif";
+
         else if (ext.equals("mail"))
             icon = "mail.gif";
         else
